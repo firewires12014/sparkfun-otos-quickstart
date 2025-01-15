4c81922d4c198b7160a45f3bc9ba2d68716f6567
The Fire Wires 12014 <firewires12014@gmail.com>
2025-01-15 14:56:41 -0500

    Update Outtake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -1,21 +1,180 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 public class Outtake {
-    public Servo extention;
+    public static double FLIP_POSITION_UP = 0;
+    public static double FLIP_POSITION_DOWN = 1;
+    public static double GRAB_POSITION_UP = 0;
+    public static double GRAB_POSITION_DOWN = 1;
+    public static double OUTTAKE_TARGET_POSITION_OUT = 360;
+    public static double OUTTAKE_TARGET_POSITION_IN = 0;
+    public static double EXTENSION_DIRECTION = 1; // Set to positive or negative depending on direction of servo
+
+    Gamepad.RumbleEffect RUMBLE_EFFECT = new Gamepad.RumbleEffect.Builder()
+            .addStep(0.0, 1.0, 500)  //  Rumble right motor 100% for 500 mSec
+            .addStep(0.0, 0.0, 300)  //  Pause for 300 mSec
+            .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
+            .addStep(0.0, 0.0, 250)  //  Pause for 250 mSec
+            .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
+            .build();
+
+    public CRServo extension;
     public Servo grab;
     public Servo flip;
+    public AnalogInput extensionSensor;
+    public double rawServoPosition; // Current servo position in degrees
+    public double posCurrent; // Current servo position in degrees
+    public double posPrevious; // Previous servo position in degrees
+    public double delta; // Change in position in degrees
 
+    //private ExtensionSensor extensionSensor; // Your sensor instance
+
+    public enum ExtensionControl {
+        IN,
+        OUT
+    }
+
+    /**
+     * Update the outtake position
+     */
+    public void update() {
+        // Calculate the current position from the sensor's voltage
+        rawServoPosition = (extensionSensor.getVoltage() / 3.3) * 360;
+        delta = rawServoPosition - posPrevious;
+        posPrevious = rawServoPosition;
+        posCurrent += delta;
+    }
+
+
+    /**
+     * Constructor for the Outtake class
+     *
+     * @param hardwareMap The hardware map from the OpMode
+     */
     public Outtake(HardwareMap hardwareMap) {
-//        extention = hardwareMap.get(Servo.class, "outtakeExtention");
-//        grab = hardwareMap.get(Servo.class, "grab");
-//        flip = hardwareMap.get(Servo.class, "flip");
+        extension = hardwareMap.get(CRServo.class, "outtakeExtension");
+        extensionSensor = hardwareMap.get(AnalogInput.class, "extensionSensor");
+        grab = hardwareMap.get(Servo.class, "grab");
+        flip = hardwareMap.get(Servo.class, "flip");
     }
 
-    public void update () {
 
+    /**
+    public Outtake.moveOuttake state = Outtake.ExtensionControl.IN;
+     * Move the outtake in or out based on the current state
+     */
+    // public void moveOuttake(ExtensionControl state) {
+    //     switch (state) {
+    //         case IN:
+    //             moveOuttakeIn();
+    //             state = Outtake.ExtensionControl.OUT;
+    //             break;
+    //         case OUT:
+    //             moveOuttakeOut();
+    //             state = Outtake.ExtensionControl.OUT;
+    //             break;
+    //     }
+    // }
 
+    /**
+     * Get the current position of the outtake
+     */
+    public double getOuttakePosition() {
+        return posCurrent;
     }
+
+    /**
+     * Move the outtake to a target position
+     *
+     * @param targetPosition The target position to move the outtake to
+     */
+    public Action moveOuttaketoPosition(double targetPosition) {
+        return new InstantAction(() -> {
+            if (getOuttakePosition() == targetPosition) {
+                stopOuttake(); // Stop Servo
+            } else if (getOuttakePosition() < targetPosition) {
+                extension.setPower(EXTENSION_DIRECTION); // Start Servo moving in
+            } else {
+                extension.setPower(-EXTENSION_DIRECTION); // Start Servo moving out
+            }
+        });
+    }
+
+    /**
+     * Move the outtake in
+     */
+    public Action moveOuttakeIn() {
+        return new InstantAction(() -> {
+            // Check if outtake is at the target position
+            if (getOuttakePosition() == OUTTAKE_TARGET_POSITION_OUT) {
+                stopOuttake(); // Stop Servo
+                //rumble(); // Start rumble to let driver know outtake is in
+            } else {
+                // Flip Outtake up and start moving in
+                flipOuttake("up");
+                // If outtake is not at the target position start moving in
+                extension.setPower(EXTENSION_DIRECTION); // Start Servo moving in
+            }
+        });
+    }
+
+    /**
+     * Move the outtake out
+     */
+    public Action moveOuttakeOut() {
+        return new InstantAction(() -> {
+            if (getOuttakePosition() == OUTTAKE_TARGET_POSITION_IN) {
+                stopOuttake(); // Stop Servo
+                // Flip Outtake down when at position
+                flipOuttake("down");
+            } else {
+                extension.setPower(-EXTENSION_DIRECTION); // Start Servo moving out
+            }
+        });
+    }
+
+    /**
+     * Stop the outtake
+     */
+    public void stopOuttake() {
+        extension.setPower(0); // Stop Servo
+    }
+
+    /**
+     * Flip the outtake up or down
+     *
+     * @param direction The direction to flip the outtake
+     */
+    public void flipOuttake(String direction) {
+        if (direction.equalsIgnoreCase("up")) {
+            flip.setPosition(FLIP_POSITION_UP);
+            grab.setPosition(GRAB_POSITION_UP);
+        } else {
+            flip.setPosition(FLIP_POSITION_DOWN);
+            grab.setPosition(GRAB_POSITION_DOWN);
+        }
+    }
+
 }
+
+/*
+    Call via:
+    if (gamepad2.a) {
+        robot.outtake.moveOuttakeIn();
+    } else if (gamepad2.b) {
+        robot.outtake.moveOuttakeOut();
+    } else {
+        robot.outtake.stopOuttake();
+    }
+*/
+
+
