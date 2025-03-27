78586cb88a2a993632553198a3ce6e46997c21a3
firewires12014 <firewires12014@gmail.com>
2025-03-27 16:06:34 -0400

    meep meeping

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TestPath.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TestPath.java
@@ -1,22 +1,15 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.InstantAction;
-import com.acmerobotics.roadrunner.ParallelAction;
-import com.acmerobotics.roadrunner.TranslationalVelConstraint;
-import com.acmerobotics.roadrunner.ProfileAccelConstraint;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 
-
-import org.rowlandhall.meepmeep.MeepMeep;
-import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
-import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class TestPath {
     public static void main(String[] args) {
+        Pose2d start = new Pose2d(7, -64, Math.toRadians(90));
         Pose2d scorePreload = new Pose2d(new Vector2d(-6, -27), Math.toRadians(90));
         Pose2d intakeSample1 = new Pose2d(new Vector2d(28.96, -39.15), Math.toRadians(36.61));
 
@@ -24,22 +17,15 @@ public class TestPath {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .splineToLinearHeading(scorePreload.position, scorePreload.heading)
-                        .splineToLinearHeading(intakeSample1.position, intakeSample1.heading)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15).build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(start)
+                        .splineToLinearHeading(scorePreload, scorePreload.heading)
+                        .setTangent(Math.toRadians(-45))
+                        .splineToLinearHeading(intakeSample1, intakeSample1.heading)
                         .build());
 
-
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
