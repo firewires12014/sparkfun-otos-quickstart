4cb1067d464e4b90f125256b214cca15e7a031c2
The Fire Wires 12014 <firewires12014@gmail.com>
2026-01-11 15:00:16 -0500

    PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,34 +1,89 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.robot.Robot;
 
+import org.firstinspires.ftc.teamcode.util.PIDFController;
+
+@Config
 public class Hardware {
     public MecanumDrive drive;
 
-    public DcMotor frontLeft;
-    public DcMotor backLeft;
-    public DcMotor frontRight;
-    public DcMotor backRight;
-    public DcMotor intake;
+    public DcMotorEx frontLeft;
+    public DcMotorEx backLeft;
+    public DcMotorEx frontRight;
+    public DcMotorEx backRight;
+    public DcMotorEx intake;
     public DcMotorEx shooter;
+    public DcMotorEx liftEH;
+    public DcMotorEx liftCH;
+    public CRServo turret;
+    public CRServo transfer1; // closest to intake
+    public CRServo transfer2; // farther from intake
+    public Servo hood;
+
+    public static double kP = 0.5;
+    public static double kD = 0.0;
+    public static double kV = 0.0004;
+    private PIDFController.PIDCoefficients pidCoef = new PIDFController.PIDCoefficients();
+    public PIDFController shooterPID;
+
+    public static boolean tuneShooter = false;
+    public boolean shoot = false;
+
+    public static double targetVelocity = 0;
 
     public Hardware(HardwareMap hardwareMap) {
-        new Hardware(hardwareMap);
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
 
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        liftEH = hardwareMap.get(DcMotorEx.class, "liftEH");
+        liftCH = hardwareMap.get(DcMotorEx.class, "liftCH");
 
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
-        frontRight.setDirection(DcMotor.Direction.FORWARD);
-        backRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
+        backLeft.setDirection(DcMotorEx.Direction.REVERSE);
+        frontRight.setDirection(DcMotorEx.Direction.FORWARD);
+        backRight.setDirection(DcMotorEx.Direction.FORWARD);
+
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+
+        transfer1 = hardwareMap.get(CRServo.class, "transfer1");
+        transfer2 = hardwareMap.get(CRServo.class, "transfer2");
+
+        turret = hardwareMap.get(CRServo.class, "turret");
+        hood = hardwareMap.get(Servo.class, "hood");
+
+        pidCoef.kP = kP;
+        pidCoef.kD = kD;
+
+        shooterPID = new PIDFController(pidCoef);
+    }
+
+    public void update() {
+        if (tuneShooter) {
+            pidCoef.kP = kP;
+            pidCoef.kD = kD;
+            shooterPID = new PIDFController(pidCoef);
+        }
+
+        shooterPID.targetPosition = targetVelocity;
+        double power = shooterPID.update(shooter.getVelocity()) + targetVelocity * kV;
+
+        if (shoot) shooter.setPower(power);
     }
 }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,128 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import static org.firstinspires.ftc.teamcode.Hardware.*;
+
+
+@Config
+@TeleOp(name="Teleop", group="Linear OpMode")
+public class Teleop extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    public static int velocity = 1800;
+    public static int hoodUpperLimit = 1;
+    public static int hoodLowerLimit = 0;
+    public static Double lift1Power = -1.0;
+    public static Double lift2Power = 1.0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        Hardware robot = new Hardware(hardwareMap);
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        robot.liftCH.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.liftCH.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            double max;
+
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral =  gamepad1.left_stick_x;
+            double yaw     =  gamepad1.right_stick_x;
+
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+            double frontLeftPower  = axial + lateral + yaw;
+            double frontRightPower = axial - lateral - yaw;
+            double backLeftPower   = axial - lateral + yaw;
+            double backRightPower  = axial + lateral - yaw;
+
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+            max = Math.max(max, Math.abs(backLeftPower));
+            max = Math.max(max, Math.abs(backRightPower));
+
+            if (max > 1.0) {
+                frontLeftPower  /= max;
+                frontRightPower /= max;
+                backLeftPower   /= max;
+                backRightPower  /= max;
+            }
+
+            // Send calculated power to wheels
+            robot.frontLeft.setPower(frontLeftPower);
+            robot.frontRight.setPower(frontRightPower);
+            robot.backLeft.setPower(backLeftPower);
+            robot.backRight.setPower(backRightPower);
+
+            if (gamepad2.right_trigger > 0) {
+                robot.intake.setPower(1);
+                robot.transfer1.setPower(1);
+                robot.transfer2.setPower(1);
+            } else {
+                robot.intake.setPower(0);
+                robot.transfer1.setPower(0);
+                robot.transfer2.setPower(0);
+            }
+
+            // Enable shooter
+//            robot.shoot = gamepad2.left_trigger > 0;
+            robot.shoot = true;
+
+            if (gamepad2.left_bumper) {
+                robot.turret.setPower(1);
+            } else if (gamepad2.right_bumper) {
+                robot.turret.setPower(-1);
+            } else {
+                robot.turret.setPower(0);
+            }
+
+            if (gamepad1.dpad_down) {
+                robot.liftCH.setPower(lift1Power);
+                robot.liftEH.setPower(lift2Power);
+            } else if (gamepad1.dpad_up) {
+                robot.liftCH.setPower(-lift1Power);
+                robot.liftEH.setPower(-lift2Power);
+            } else {
+                robot.liftCH.setPower(0);
+                robot.liftEH.setPower(0);
+            }
+
+            if (gamepad2.dpad_up) {
+                robot.hood.setPosition(hoodUpperLimit);
+            }
+
+            if (gamepad2.dpad_down) {
+                robot.hood.setPosition(hoodLowerLimit);
+            }
+
+            robot.update();
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Velocity", robot.shooter.getVelocity());
+            telemetry.addData("Target Velo", targetVelocity);
+            telemetry.addData("Turret Encoder: ", robot.liftEH.getCurrentPosition());
+            telemetry.addData("Zero", 0);
+            telemetry.update();
+        }
+    }
+}
+
