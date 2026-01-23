7ec8f73f59ab5060a3c23f04d980fbf377ec3060
Richard Lester <hardcopi@yahoo.com>
2026-01-23 09:27:20 -0500

    Shooter Subsystem Updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -12,6 +12,7 @@ import org.firstinspires.ftc.teamcode.subsystems.Hood;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Transfer;
 import org.firstinspires.ftc.teamcode.subsystems.Turret;
+import org.firstinspires.ftc.teamcode.subsystems.Shooter;
 
 @TeleOp(name = "Teleop", group = "Linear OpMode")
 public class Teleop extends LinearOpMode {
@@ -27,6 +28,7 @@ public class Teleop extends LinearOpMode {
         Transfer transfer = new Transfer(hardwareMap);
         Turret turret = new Turret(hardwareMap);
         Hood hood = new Hood(hardwareMap);
+        Shooter shooter = new Shooter(hardwareMap);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -35,7 +37,6 @@ public class Teleop extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-        double targetVel = 0.0;
         ElapsedTime loopTimer = new ElapsedTime();
         loopTimer.reset();
 
@@ -51,24 +52,7 @@ public class Teleop extends LinearOpMode {
 
             // Smooth ramp for shooter velocity:
             double triggerVal = gamepad2.left_trigger;
-            if (triggerVal > 0.001) {
-                targetVel = Constants.SHOOTER_VELOCITY;
-                transfer.triggerOpen();
-                if (drive.shooter.getVelocity() >= Constants.SHOOTER_VELOCITY) {
-                    intake.in();
-                    transfer.run();
-                }
-            } else {
-                transfer.triggerClose();
-                targetVel = 0;
-            }
-
-            drive.shooter.setVelocity(targetVel);
-
-//            if (gamepad1.circle) {
-//                transfer.triggerClose();
-//                telemetry.addLine("Trigger closed");
-//            }
+            shooter.update(triggerVal > 0.001);
 
             if (gamepad2.cross && triggerVal == 0) {
                 intake.in();
@@ -77,19 +61,19 @@ public class Teleop extends LinearOpMode {
                 intake.out();
                 transfer.reverse();
                 drive.shooter.setPower(-.5);
-            } else if (gamepad2.right_trigger > 0  && triggerVal == 0) {
+            } else if (gamepad2.right_trigger > 0 && triggerVal == 0) {
                 intake.in();
                 transfer.stop();
                 transfer.triggerClose();
                 telemetry.addLine("Trigger closed");
-            }  else if (gamepad2.right_trigger == 0  && triggerVal == 0) {
+            } else if (gamepad2.right_trigger == 0 && triggerVal == 0) {
                 intake.stop();
                 transfer.stop();
             }
-//            else  {
-//                intake.stop();
-//                transfer.stop();
-//            }
+            // else {
+            // intake.stop();
+            // transfer.stop();
+            // }
 
             // Transfer runs only when shooting
             if (triggerVal > 0.001) {
@@ -112,15 +96,12 @@ public class Teleop extends LinearOpMode {
                 hood.down();
             }
 
-
-
             drive.update();
 
             telemetry.addData("Status", "Run Time: " + runtime);
             telemetry.addData("Velocity", drive.shooter.getVelocity());
-            telemetry.addData("Target Velo", targetVel);
+            telemetry.addData("Target Velo", (triggerVal > 0.001) ? Constants.SHOOTER_VELOCITY : 0.0);
             telemetry.update();
         }
     }
 }
-
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -6,14 +7,27 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.Hardware;
 
 public class Shooter extends Hardware {
-//    Transfer transfer = new Transfer(hardwareMap);
+    // Transfer transfer = new Transfer(hardwareMap);
     public Shooter(HardwareMap hardwareMap) {
         super(hardwareMap);
     }
 
-
-    public void shoot (double velocity) {
+    public void shoot(double velocity) {
         shooter.setVelocity(velocity);
     }
-}
 
+    public void update(boolean shoot) {
+        if (shoot) {
+            shooter.setVelocity(org.firstinspires.ftc.teamcode.Constants.SHOOTER_VELOCITY);
+            trigger.setPosition(org.firstinspires.ftc.teamcode.Constants.TRIGGER_OPEN);
+
+            if (shooter.getVelocity() >= org.firstinspires.ftc.teamcode.Constants.SHOOTER_VELOCITY) {
+                intake.setPower(1);
+                transfer.setPower(org.firstinspires.ftc.teamcode.Constants.TRANSFER_SPEED);
+            }
+        } else {
+            shooter.setVelocity(0);
+            trigger.setPosition(org.firstinspires.ftc.teamcode.Constants.TRIGGER_CLOSE);
+        }
+    }
+}
